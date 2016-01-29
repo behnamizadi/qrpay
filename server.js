11c4a0b3db657574cd0883fe66fe2e0551e19033
behnamizadi <behnamizadi@yahoo.com>
2016-01-29 21:51:58 +0330

    some fixex

--- server.js
+++ server.js
@@ -1,12 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'https://qrap.herokuapp.com/*');
-  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  return next();
-});
+app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res){
   res.sendfile('index.html');
 });
