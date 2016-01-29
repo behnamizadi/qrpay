0fb0d80067c8dfa6d2318282b1111649ca98615c
behnamizadi <behnamizadi@yahoo.com>
2016-01-29 21:31:01 +0330

    some fixex

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  return next();
+});
 app.get('/', function(req, res){
   res.sendfile('index.html');
 });
