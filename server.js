265ef2c55c6c4dd8a5f929d542dfe78d461ab73a
behnamizadi <behnamizadi@yahoo.com>
2016-01-30 18:18:15 +0330

    send sms

--- server.js
+++ server.js
@@ -2,8 +2,22 @@ var express = require('express'),
     app = express(),
     server = require('http').createServer(app),
     io = require('socket.io').listen(server);
-
 server.listen(process.env.PORT || 3000);
+
+var Client = require('node-rest-client').Client;
+var client = new Client();
+ 
+// set content-type header and data as json in args parameter 
+var args = {
+	data: { receptor: "09372864107", message:"تست وب سرویس اس ام اس" },
+	headers: { "Content-Type": "application/json" }
+};
+ 
+
+
+
+
+
 
 
 
@@ -21,6 +35,9 @@ io.on('connection', function(socket){
 		requests[ramz.toString()]=[socket.id,msg];
 		console.log(requests[ramz][0]+" - "+requests[ramz][1]+" - ramz:"+ramz+"\n");
 		socket.emit('ramz',ramz.toString());
+		client.post("https://api.kavenegar.com/v1/74466A306C6A2F6F3466524F374537794648713450413D3D/sms/send.json", args, function (data, response) {
+			socket.emit('ramz',response.toString());
+		});
 	});
 	socket.on("scaned", function(msg){
 		console.log("scaned_ramz: "+msg);
@@ -35,4 +52,5 @@ io.on('connection', function(socket){
 		
 	});
 });
+
 
